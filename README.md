fd0e7d8e5b4414fb813748804d4fc47b220519c8
Johnny Neagoe <johnnyneagoe@gmail.com>
2018-08-21 23:41:27 -0400

    04

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # bamazon
 MySQL and Node.js App
+# Customer Perspective (Part 1)
+## The video links below will walk you through how part one of the application works. 
+### The Application will be run in the terminal window using node, with the quantities of each product's inventory, in MySQL, changing as the customer makes purchases.
 
-## The Vide Link Below will walk you through how the application works. 
-###The Application will be run in the terminal using node. With the quantities of the invetory chnging as the customer makes purchases
+link to demo: [I'm an inline-style link with title](https://drive.google.com/open?id=1n0-4BtiwGwWcnr-WOP8Y1BQXD0yqBsmx "Demo of Customer")
 
 
-link to demo: https://drive.google.com/open?id=1n0-4BtiwGwWcnr-WOP8Y1BQXD0yqBsmx
+# Manager Perspective (Part 2)
+## The video links below will walk you through how part two the application works. 
+### The Application will be run in the terminal window using node, allowing the manager to make changes to the inventory of product's available as they see fit. 
+
+link to demo: [I'm an inline-style link with title](https://drive.google.com/open?id=1n0-4BtiwGwWcnr-WOP8Y1BQXD0yqBsmx "Demo of Manager)
+
+
--- bamazonManager.js
+++ bamazonManager.js
@@ -7,6 +7,11 @@ const connection = mysql.createConnection({
     password: "root",
     database: "bamazon"
 });
+let oldQuantity;
+let idSelected;
+let productName;
+let newQuantity;
+
 connection.connect(function(err,res){
     if (err) throw err
     console.log("connected as id " + connection.threadId + "\n");
@@ -45,24 +50,109 @@ function managerMenu() {
 
 function productSearch(){
     console.log("Displaying all Products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
+    let query = "SELECT * FROM products"
+    connection.query(query, function(err, res) {
+        console.log("\n")
         for (var i = 0; i<res.length; i++){
             console.log(res[i].item_id + " | " + "Name: " + res[i].products_name + " | " + "Department: " +  res[i].department_name + " | " + "Price: " + "$" + res[i].price + " | " + "Stock Available: " +  res[i].stock_quantity + " | ");
         }
+        console.log("\n")
         managerMenu();
     });
 };
 
 function lowInventoryCount(){
-    connection.query("SELECT * FROM products WHERE stock_quantity", function(err, res) {
-        if (res.stock_quantity < 5){
-            console.log(res.stock_quantity)
+    let query = "SELECT * FROM products WHERE stock_quantity < 5"
+    connection.query(query, function(err, res) {
+        if (res.length < 1) {  
+            console.log("There are no products with an inventory of less then 5 items!")
+        }
+        else { 
+            console.log("\n")
             for (var i = 0; i<res.length; i++){
                 console.log(res[i].item_id + " | " + "Name: " + res[i].products_name + " | " + "Department: " +  res[i].department_name + " | " + "Price: " + "$" + res[i].price + " | " + "Stock Available: " +  res[i].stock_quantity + " | ");
-            }
+            } 
+        } 
+        console.log("\n")
+               managerMenu()
+    }); 
+};
 
-        } else console.log("Full capacity")
+function addToInventory(){
+    inquirer
+    .prompt([
+    {
+        name: "product",
+        type: "input",
+        message: "What is the ID of the product undergoing an inventory change?"
+    },
+    {
+        name: "add",
+        type: "input",
+        message: "How many units will be added?"
 
-
-        });
+    }]).then(function(manager){
+        newQuantity = parseFloat(manager.add);
+        idSelected = manager.product;
+            connection.query("SELECT * FROM products WHERE ?", {item_id: idSelected}, function(err, res){
+                oldQuantity = res[0].stock_quantity;
+                productName = res[0].products_name;
+                updateProduct();
+            });
+    });
+    function updateProduct(){
+        let query = "UPDATE products SET ? WHERE ?";
+        connection.query(query,[ {stock_quantity: oldQuantity + newQuantity,},{item_id: idSelected,} ], function(err, res){
+            console.log("\n");
+            console.log(res.affectedRows + " One Product has Been Updated!")
+            console.log(newQuantity + " units of " + productName + " have been added");
+            console.log("\n");
+            managerMenu();
+        })
     }
+}
+
+function addNewProduct(){
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter new product name"
+        },
+        {
+            type: "input",
+            name: "department",
+            message: "Please enter department for new products"
+        },
+        {
+            type: "input",
+            name: "price",
+            message: "Please enter the new product's cost per unit (number only, no characters)"
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "Please enter current inventory of new product"
+        }
+    ]).then(function(newProduct){
+        let newName = newProduct.name;
+        let newDepartment = newProduct.department;
+        let newPrice = newProduct.price;
+        let newQuantity = newProduct.quantity;
+        let query = "INSERT INTO products SET ?";
+        connection.query(query, 
+        {
+            products_name: newName ,
+            department_name: newDepartment ,
+            price: newPrice,
+            stock_quantity: newQuantity
+        },
+        function(err, res){
+            console.log("\n");
+            console.log(res.affectedRows + " Product added to bamazon!");
+            console.log("\n");
+            managerMenu();
+        })
+    });
+}
